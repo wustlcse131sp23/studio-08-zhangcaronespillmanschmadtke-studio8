efb16dffa6581b5cd2ca6744b481df8882f8e5f7
josephzhang <josephzhang@localhost>
2023-04-06 12:41:41 -0500

    Let's go let's go let's go

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,7 +1,34 @@
 package studio8;
 
-public class Date {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Date {
+	
+	private int month;
+	private int date;
+	private int year;
+	private Boolean isHoliday;
+	private List<Date> dates = new LinkedList<Date>();
+	
+	public Date(int newMonth, int newDate, int newYear, Boolean holidayStatus)
+	{
+		this.month = newMonth;
+		this.date = newDate;
+		this.year = newYear;
+		this.isHoliday = holidayStatus;
+		
+	}
+	
+	public int hashCode()
+	{
+		return 0;
+	}
+	
+	public String toString()
+	{
+		return month + "." + date + "." + year + " Holiday: " + isHoliday + ".";
+	}
 
     public static void main(String[] args) {
 
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,7 +2,39 @@ package studio8;
 
 
 public class Time {
-
+	
+	private int hour;
+	private int minute;
+	private Boolean is12Hours;
+	
+	public Time(int hours, int minutes, Boolean is12Hour)
+	{
+		this.hour = hours;
+		this.minute = minutes;
+		this.is12Hours = is12Hour;
+	}
+	
+	public String toString()
+	{
+		if (is12Hours = true)
+		{
+			if (hour > 12)
+			{
+				return (hour - 12) + ":" + minute + " PM";
+			}
+			else
+			{
+				return hour + ":" + minute + " AM";
+			}
+				
+		}
+		else
+		{
+			return hour + ":" + minute;
+		}
+			
+	}
+	
 	public static void main(String[] args) {
  
     	
